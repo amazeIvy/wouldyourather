5a7db9daf82c7ed5994cc8896dc08ef606062c8b
Ivy Fu <ivy.fu@vendhq.com>
2018-09-04 18:07:03 +1200

    Load userList depends on users in store, and add handler to setAuthedUser

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { setAuthedUser } from '../actions/authedUser'
 
 class Login extends Component {
+  state = {
+    selectedUserId: ''
+  }
+
+  handleLogIn = (e) => {
+    e.preventDefault();
+
+    const { dispatch } = this.props;
+    const { selectedUserId } = this.state;
+
+    dispatch(setAuthedUser(selectedUserId))
+  }
+
+  handleSelectUser = (e) => {
+    const id = e.target.value
+
+    this.setState (() => ({
+      selectedUserId: id
+    }))
+  }
+
   render () {
+    const { users } = this.props;
+
     return (
       <div className="center log-in-container">
         <h3>Welcome to Would You Rather<br />Please Log In to Continue</h3>
@@ -11,13 +37,16 @@ class Login extends Component {
             alt="would you red velvet" />
         </div>
         <div className="log-in-box">
-          <select defaultValue="default">
+          <select defaultValue="default" onChange={this.handleSelectUser}>
             <option value="default" disabled>Select User</option>
-            <option value="sarahedo">Sarah Edo</option>
-            <option value="tylermcginnis">Tyler McGinnis</option>
+            {
+              users.map(user => (
+                <option key={ user.id } value={ user.id }>{ user.name }</option>
+              ))
+            }
           </select>
         </div>
-        <button className="btn">
+        <button className="btn" onClick={this.handleLogIn}>
           Log In
         </button>
       </div>
@@ -25,4 +54,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+function mapStateToProps ({ users }) {
+  return {
+    users: Object.keys(users).map(userId => {
+      return {
+        id: userId,
+        name: users[userId].name
+      }
+    })
+  };
+}
+
+export default connect(mapStateToProps)(Login);
